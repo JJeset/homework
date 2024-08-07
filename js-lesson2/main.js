e6bb48eb80abf8a1891bb2bdafc9605094a3128f
JJeset <arkhip57@gmail.com>
2024-08-07 23:33:13 +0300

    Delete js-lesson2 directory

--- js-lesson2/main.js
+++
@@ -1,9 +0,0 @@
-const age = 28
-
-if (age >= 18) {
-    console.log('Вы совершеннолетний')
-} else {
-    console.log('Вам еще нет 18')
-}
-
-age >= 18? console.log('Вы совершеннолетний') : console.log('Вам еще нет 18')
